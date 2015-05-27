f93644ffc9f0b0173e0f657c8c7c5803ba649ac2
king_lamer <araxnid@mail.ru>
2015-05-27 11:49:46 +0300

    Вынос переменных

--- AutoCreateBackupPlan/Standart/ClassConstHelper.cs
+++ AutoCreateBackupPlan/Standart/ClassConstHelper.cs
@@ -13,6 +13,21 @@ namespace AutoCreateBackupPlan.Standart
         public static readonly string profileName = "King.Profile";
         public static readonly string accountName = "King.Account";
 
+
+        /// <summary>
+        /// У каждого созданного файла бэкапа, будет ставиться временная метка, когда он был создан, с точностью до секунд
+        /// </summary>
+        public static readonly string formatTimeStampFiles = "REPLACE(REPLACE(convert(varchar,GETDATE(), 126),'':'',''_'') ,''.'',''_'')";
+
+        /// <summary>
+        /// Имена создаваемых реземерных копий баз данных 
+        /// </summary>
+        public static readonly string masterdb_name = "''MASTER_''";
+        public static readonly string msdb_name = "''MSDB_''";
+        public static readonly string diff_name = "''KING_DIF_''";
+        public static readonly string full_name = "''KING_FULL_''";
+        public static readonly string trab_name = "''KING_TRAN_''";
+        
         public static string DB { get; set; }
         public static string serverSQL { get; set; }
 
