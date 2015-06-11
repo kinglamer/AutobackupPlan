247faa2e343ff1214c7f6bd39b4e2c0c9619fd31
king_lamer <araxnid@mail.ru>
2015-06-11 17:20:29 +0300

    Доработка стать, мелкие правки кода

--- AutoCreateBackupPlan/Standart/frmStandart.designer.cs
+++ AutoCreateBackupPlan/Standart/frmStandart.designer.cs
@@ -168,7 +168,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(674, 363);
+            this.ClientSize = new System.Drawing.Size(674, 379);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.btSetupDatabaseMail);
             this.Controls.Add(this.btClose);
